f17de4b7cfc714c9c0d758a7cb0a86455eef1a41
Timothy Andrews <75810156+ta440@users.noreply.github.com>
2025-05-20 14:44:24 -0400

    Create regrid.sh

---
+++ other_scripts/regrid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export srcgrid=$1
+export dstgrid=$2
+
+export griddir=~/remapping/
+export file=$3
+
+export srcinitfile=${file}.nc
+export dstinitfile=${file}.regrid.${dstgrid}.nc
+
+ncremap -m ${griddir}map_${srcgrid}_to_${dstgrid}_aave.nc -i\
+ ${srcinitfile} -o ${dstinitfile}
